a3962220a5c4ee87a5b88a9595bf5ba7af449c2c
POMAH <77603353+POHAM1209@users.noreply.github.com>
2023-10-17 11:49:25 +0800

    Update DefectDetection.cpp

--- CPlusPlus/CPlusPlus/DefectDetection.cpp
+++ CPlusPlus/CPlusPlus/DefectDetection.cpp
@@ -2,6 +2,13 @@
 
 namespace PZTIMAGE {
 
+	bool MiniLEDPanelDetection(IntPtr t_imgPtr, ImageMeta t_imgMeta, FunctionOption t_opt, std::vector<Defect>& t_res){
+		bool res = false;
+		t_res.clear();
+
+		return res;
+	}
+
 	bool TestDefectDetection() {
 		bool res = false;
 
