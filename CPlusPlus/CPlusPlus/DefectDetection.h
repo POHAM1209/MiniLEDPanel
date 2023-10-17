3c0fcc33f9c7815ad96b8620af827d5474eebcac
POMAH1209 <411970028@qq.com>
2023-10-17 11:44:38 +0800

    Updata DefectDetection.h

--- CPlusPlus/CPlusPlus/DefectDetection.h
+++ CPlusPlus/CPlusPlus/DefectDetection.h
@@ -1,13 +1,40 @@
 #ifndef _DEFECTDETECTION_H_
 #define _DEFECTDETECTION_H_
 
-#if
+#define IntPtr long long*
 
 // 
 namespace PZTIMAGE {
+	// define enum
+	
+	enum ImageFlag{IMAGEFLAG_MONO8 = 0, IMAGEFLAG_RGB8};
+	enum DefectType{DEFECTTYPE_POINT = 0, DEFECTTYPE_LINE};
 
+	// define struct
+	
+	typedef struct ImageMeta{
+		IntPtr       m_imgPtr;
+		unsigned int m_imgW;
+		unsigned int m_imgH;
+		ImageFlag    m_flag;
+	}ImageMeta;
+	
+	typedef struct FunctionOption{
+	
+	}FunctionOption;
+
+	typedef struct Defect{
+	
+		DefectType   m_type;
+		unsigned int m_area;
+	}Defect;
+
+	// brief : 
+	// param0: 
+	// return: 
+	bool MiniLEDPanelDetection(IntPtr t_imgPtr, ImageMeta t_imgMeta, FunctionOption t_opt, std::vector<Defect>& t_res);
+	
 	bool TestDefectDetection();
-
 }
 
 #endif
