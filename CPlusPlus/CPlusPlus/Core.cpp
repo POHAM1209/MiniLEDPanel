e115f26bef586c8e2b54253a7e019ac6902e7de8
POMAH1209 <411970028@qq.com>
2023-10-18 21:22:32 +0800

    vs

--- CPlusPlus/CPlusPlus/Core.cpp
+++ CPlusPlus/CPlusPlus/Core.cpp
@@ -8,7 +8,7 @@ namespace PZTIMAGE {
 	
 	PZTImage::PZTImage(const std::string& t_fileName){
 		m_container = cv::imread(t_fileName, cv::IMREAD_UNCHANGED);
-
+		m_mark = cv::Mat(m_container.rows(), m_container.cols(), cv::)
 	}
 
 	PZTImage& PZTImage::operator=(const PZTImage& t_other){
--- CPlusPlus/CPlusPlus/Core.h
+++ CPlusPlus/CPlusPlus/Core.h
@@ -1,7 +1,7 @@
 #ifndef _CORE_H_
 #define _CORE_H_
 
-#include <opencv2/core.h>
+#include <opencv2/core.hpp>
 
 #include <string>
 #include <vector>
