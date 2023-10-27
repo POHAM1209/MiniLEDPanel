803cecbc9707a1139f8c1d241ff8088577cde3ef
POMAH1209 <411970028@qq.com>
2023-10-27 12:17:20 +0800

    Updata the interface of PZTImage and PZTRegion

--- CPlusPlus/CPlusPlus/Core.cpp
+++ CPlusPlus/CPlusPlus/Core.cpp
@@ -10,18 +10,18 @@ namespace PZTIMAGE {
 	}
 	
 	PZTImage::PZTImage(const std::string& t_fileName){
-		m_container = cv::imread(t_fileName, cv::IMREAD_ANYCOLOR);
-		m_mark = cv::Mat(m_container.rows, m_container.cols, CV_8UC3, cv::Scalar(255));
+		m_image= cv::imread(t_fileName, cv::IMREAD_ANYCOLOR);
+		m_mask = cv::Mat(m_image.rows, m_image.cols, CV_8UC1, cv::Scalar(1));
 	}
 
 	PZTImage::PZTImage(const PZTImage& t_other){
-		m_container = t_other.m_container;
-		m_mark = t_other.m_mark;
+		m_image = t_other.m_image;
+		m_mask = t_other.m_mask.clone();
 	}
 
 	PZTImage& PZTImage::operator=(const PZTImage& t_other){
-		m_container = t_other.m_container;
-		m_mark = t_other.m_mark;
+		m_image = t_other.m_image;
+		m_mask = t_other.m_mask.clone();
 		return *this;
 	}
 
@@ -29,27 +29,65 @@ namespace PZTIMAGE {
 	
 	}
 
+	bool PZTImage::GetImageSize(unsigned int& t_imgRow, unsigned int& t_imgCol){
+		bool res = false;
+
+		return res;
+	}
+
+	bool PZTImage::Compose(const cv::Mat& t_ch0, const cv::Mat& t_ch1, const cv::Mat& t_ch2){
+		bool res = false;
+
+		return res;
+	}
+
+	bool PZTImage::Decompose(cv::Mat& t_ch0, cv::Mat& t_ch1, cv::Mat& t_ch2){
+		bool res = false;
+
+		return res;
+	}
+
+	bool PZTImage::ReduceDomain(const PZTRegions& t_reg){
+		bool res = false;
+
+		return res;
+	}
+
+	bool PZTImage::RGB2Gray(){
+		bool res = false;
+
+		return res;
+	}
+
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// PZTRegions
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	PZTRegions::PZTRegions() {
-
+	PZTRegions::PZTRegions():
+		m_regions(),
+		m_regionNum(0),
+		m_featuresPtr()
+	{
+		
 	}
 
-	PZTRegions::PZTRegions(cv::InputArray t_regI){
-		m_container = t_regI.getMat();
-		
-		// confirm mat::type == CV_16U
-
+	PZTRegions::PZTRegions(cv::Mat t_reg) :
+		m_regions(),
+		m_regionNum(0)
+	{
 		_UpdataRegionNum();
-		_UpdataRegionFeatures();
+		m_featuresPtr = std::make_shared<std::vector<RegionFeature>>(m_regionNum);
 	}
 
 	PZTRegions::PZTRegions(const PZTRegions& t_other) {
-
+		m_regions = t_other.m_regions;
+		m_featuresPtr = t_other.m_featuresPtr;
+		m_regionNum = t_other.m_regionNum;
 	}
 
 	PZTRegions& PZTRegions::operator= (const PZTRegions& t_other) {
+		m_regions = t_other.m_regions;
+		m_featuresPtr = t_other.m_featuresPtr;
+		m_regionNum = t_other.m_regionNum;
 
 		return *this;
 	}
@@ -59,40 +97,87 @@ namespace PZTIMAGE {
 	}
 
 	RegionFeature PZTRegions::GetRegionFeature(unsigned int t_index) {
-		if (t_index > m_regNum)
+		// confirm
+		if(t_index > m_regionNum)
 			return RegionFeature();
-		else
-			return m_features[t_index];
+
+		int size = m_featuresPtr->size();
+		if(size == 0 || size != m_regionNum)
+			bool res = _UpdataRegionFeatures();
+
+		return (*m_featuresPtr)[t_index];
 	}
 
 	bool PZTRegions::Connection() {
-		if (m_regNum != 1)
+		// confirm
+		if(m_regions.data == nullptr)
 			return false;
 
-		cv::connectedComponents(m_container, m_container, 8, CV_16U);
-		_UpdataRegionNum();
-		_UpdataRegionFeatures();
+		cv::Mat tmp16, tmp8;
+		cv::threshold(m_regions, tmp16, 0, 255, cv::THRESH_BINARY);
+		tmp16.convertTo(tmp8, CV_8U);
+		m_regionNum = cv::connectedComponents(tmp8, m_regions, 8, CV_16U);
+	
+		return true;
+	}
+
+	bool PZTRegions::Disconnection(){
+		// confirm
+		if(m_regions.data == nullptr)
+			return false;
+		
+		m_regionNum = 1;
+		cv::threshold(m_regions, m_regions, 0, 1, cv::THRESH_BINARY);
+
+		return true;
+	}
+
+	bool PZTRegions::Erosion(StructElement t_elm){
+		
+		return true;
 	}
 
 	bool PZTRegions::_UpdataRegionNum(){
 		double maxValue = 0;
-		cv::minMaxLoc(m_container, &maxValue, nullptr, nullptr, nullptr);
-		m_regNum = maxValue;
+		cv::minMaxLoc(m_regions, &maxValue, nullptr, nullptr, nullptr);
+		m_regionNum = maxValue;
 		
 		return true;
 	}
 
 	bool PZTRegions::_UpdataRegionFeatures(){
-		m_features.clear();
-		m_features.reserve(m_regNum);
+		m_featuresPtr->clear();
+		m_featuresPtr->reserve(m_regionNum);
 		
 		//....
-
+		std::vector<std::vector<cv::Point>> contours;
+		cv::Mat tmp16, tmp8;
+		cv::threshold(m_regions, tmp16, 0, 255, cv::THRESH_BINARY);
+		tmp16.convertTo(tmp8, CV_8U);
+		//cv::findContours(m_regions, contours, )
+		// 做findCoontours的实验
+		
 		return true;
+	}
+
+	bool PZTRegions::_UpdataRegions(){
+		//cv::threshold(m_regions, )
+
+		return false;
 	}
 	
 	bool TestCore() {
 		bool res = false;
+
+		cv::Mat img = cv::imread(std::string("E:/gray.png"), cv::IMREAD_GRAYSCALE);
+		
+		std::cout << "   ---    " << (int)(img.data[60]) << "\n";
+
+
+		auto imgCpy = img;
+		cv::pow(imgCpy, 2, imgCpy);
+
+		std::cout << (int)(imgCpy.data[60]) << "   ---    " << (int)(img.data[60]);
 
 		return res;
 	}
--- CPlusPlus/CPlusPlus/Core.h
+++ CPlusPlus/CPlusPlus/Core.h
@@ -3,69 +3,149 @@
 
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 
 #include <string>
 #include <vector>
+#include <memory>
+#include <iostream>
 
-namespace PZTIMAGE{
+namespace PZTIMAGE {
 
+	class PZTRegions;
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// brief:
 	// 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	typedef struct RegionFeature{
+	enum StructElement { STRUCTELEMENT_CIRCLE, STRUCTELEMENT_RECTANGLE };
+
+	typedef struct RegionFeature {
 		unsigned int 			m_area;
 		unsigned int 			m_row;
 		unsigned int 			m_column;
-		
+
 	}RegionFeature;
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// brief:
+	// m_image is CV_8U; m_mask is CV_8UC1 
+	//
+	// m_mask copyTo
+	// cv::Mat::refcount refers to the shared memory.   
+	// PZTImage a = b; --> they(a, b) refer to the same memory.
 	// 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	class PZTImage{
+	class PZTImage {
+		friend class OperatorSet;
 	public:
 		PZTImage();
-		PZTImage(const std::string& t_fileName);
 		PZTImage(const PZTImage& t_other);
 		PZTImage& operator = (const PZTImage& t_other);
 		~PZTImage();
 
+		/* 
+		* param0[i]: The path of the image to be read.
+		*/
+		PZTImage(const std::string& t_filePath);
+		
+	public:
+		/* 
+		* brief    : Return the size of an image.
+		* param0[o]: The height of an image.
+		* param1[o]: The width of an image.
+		*/
+		bool GetImageSize(unsigned int& t_imgRow, unsigned int& t_imgCol);
+
+		/* 
+		* brief    : Convert 3 images with one channel into a three-channel image.
+		* param0[i]: The 1st channel.
+		* param1[i]: The 2nd channel.
+		* param2[i]: The 3rd channel.
+		*/
+		bool Compose(const cv::Mat& t_ch0, const cv::Mat& t_ch1, const cv::Mat& t_ch2);
+
+		/* 
+		* brief    : Convert a three-channel image into three images with one channel.
+		* param0[o]: The 1st channel.
+		* param1[o]: The 2nd channel.
+		* param2[o]: The 3rd channel.
+		*/
+		bool Decompose(cv::Mat& t_ch0, cv::Mat& t_ch1, cv::Mat& t_ch2);
+
+		/* 
+		* brief    : Reduce the image with reduced definition domain.
+		* param0[i]: New definition domain.
+		*/
+		bool ReduceDomain(const PZTRegions& t_reg);
+
+		/* 
+		* brief    : Transform an RGB image into a gray scale image.
+		*/
+		bool RGB2Gray();
+
 	private:
-		cv::Mat m_container;
-		cv::Mat m_mark;
+		cv::Mat 										m_image;
+		cv::Mat 										m_mask;
 	};
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// brief:
 	// 
+	// The flags of input array in cv::threshold and cv::connectedComponents is CV_8U
+	//
+	// the image processing operator related with Halcon Region includes 
+	// 	-- threshold(region)
+	//	-- erosion_xxx/dilation_xxx(region array)
+	//	-- select_shape(region array)
+	//	-- union1(region array)
+	//	-- union2(region array)
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	class PZTRegions{
+	class PZTRegions {
 	public:
 		PZTRegions();
-		PZTRegions(cv::InputArray t_regI); // 输入图片有问题
 		PZTRegions(const PZTRegions& t_other);
 		PZTRegions& operator= (const PZTRegions& t_other);
 		~PZTRegions();
 
+		/*
+		* param0[i]: labeled region. The grayscale value i represents the i-th connected domain.
+		*/
+		PZTRegions(cv::Mat t_reg);
+
 	public:
+		/* 
+		* brief    : Return features of a connected domain.
+		* param0[i]: The index of connected domain.
+		*/
 		RegionFeature GetRegionFeature(unsigned int t_index);
 
+		/*
+		* brief    : Compute connected components of a region.
+		*/
 		bool Connection();
+
+		/*
+		* brief    : Gain the union of all connected domains.
+		*/
+		bool Disconnection();
+
+		/*
+		* brief    : Erode a region
+		* param0[i]: Structuring element, such as rectangle, circle.
+		*/
+		bool Erosion(StructElement t_elm);
 
 	private:
 		bool _UpdataRegionNum();
 		bool _UpdataRegionFeatures();
-	
+		bool _UpdataRegions();
+
 	private:
-		// CV_16UC1
-		cv::Mat      					m_container;
-		//
-		unsigned int 					m_regNum;
-		// 
-		std::vector<RegionFeature> 		m_features;
+		// Mat::flags is CV_8UC1
+		cv::Mat											m_regions;
+		std::shared_ptr<std::vector<RegionFeature>>		m_featuresPtr;
+		unsigned int 									m_regionNum;
 	};
 
 	bool TestCore();
