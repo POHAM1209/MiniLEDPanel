90e9ce460a543632d0291b4de28d75a5ed16e034
Lizk595 <148237512+Lizk595@users.noreply.github.com>
2023-11-07 21:04:39 +0800

    Update Test.cpp
    Updated on November 7th

--- CPlusPlus/CPlusPlus/Test.cpp
+++ CPlusPlus/CPlusPlus/Test.cpp
@@ -4,14 +4,16 @@ namespace PZTIMAGE {
 
 	bool TestTest() {
 		bool res = false;
-		//时间测试
-		auto start_time = high_resolution_clock::now(); // 记录程序开始时间  
-		// 测试代码  
+		
+		//Test Time
+		auto start_time = chrono::high_resolution_clock::now(); // 记录程序开始时间  
+		//Code Start
 	
 
-		//代码结束
-		auto end_time = high_resolution_clock::now(); // 记录程序结束时间  
-		double duration = duration_cast<microseconds>(end_time - start_time).count(); // 计算程序运行时间（以微秒为单位）  
+		
+		//Code End
+		auto end_time = chrono::high_resolution_clock::now(); // 记录程序结束时间  
+		double duration = chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count(); // 计算程序运行时间（以微秒为单位）  
 		cout << endl << endl<<"程序运行时间：" << duration/1000000 << " 秒" << endl<<endl<<endl;
 
 		return res;
