68ca9662d7dfecb60082e63b112e00ff542121f0
POMAH1209 <411970028@qq.com>
2023-12-28 10:10:15 +0800

    liang

--- CPlusPlus/CPlusPlus/DefectDetection.h
+++ CPlusPlus/CPlusPlus/DefectDetection.h
@@ -10,14 +10,14 @@
 namespace PZTIMAGE {
 	// define enum
 	
-	enum ImageFlag{IMAGEFLAG_MONO8 = 0, IMAGEFLAG_RGB8, IMAGEFLAG_RGB16};
+	enum ImageFlag{IMAGEFLAG_MONO8 = 0, IMAGEFLAG_RGB8};
 	enum DefectType{DEFECTTYPE_POINT = 0, DEFECTTYPE_LINE};
 
 	// define struct
 	
 	// 图像
 	typedef struct ImageMeta{
-		unsigned int*  m_imgPtr;
+		unsigned char*  m_imgPtr;
 		unsigned int 	m_imgW;
 		unsigned int 	m_imgH;
 		ImageFlag    	m_flag;
@@ -39,9 +39,6 @@ namespace PZTIMAGE {
 	// return: 
 	bool MiniLEDPanelDetection(ImageMeta t_imgMeta, FunctionOption t_opt, std::vector<Defect>& t_res);
 	
-	//dong	分类不做，图像格式16U，功能不设
-	bool MiniLEDPanelDetectionV1(ImageMeta t_imgMeta);
-
 	bool TestDefectDetection();
 }
 
